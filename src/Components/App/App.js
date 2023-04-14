5a6895fe6aa3aab359e358fecb6207501ead88d7
davidwrossiter <davewrossiter@gmail.com>
2023-04-14 16:08:49 +0100

    google image functionality

---
+++ src/Components/App/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+function App() {
+    let myCase = "17xgEYkjdnxOraGrZkz2R7q7DBzi1fqcP"; 
+    return(
+        <img src={`https://drive.google.com/uc?export=view&id=${myCase}`} alt="drive image"/>
+        );
+}
+
+export default App;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className="header">
+
+            <div id="div-header-one"><p id="the-best">THE BEST</p></div>
+            <div id="div-header-two">
+
+                <div>
+                    <p></p>
+                    <img href="https://drive.google.com/drive/u/0/folders/1nscHSKDddTGMeSf67s6nu1Pfx8xyjGqM"></img>
+                </div>
+
+                <div>
+                    <p></p> 
+                    <img></img>
+                </div>
+
+                <div>
+                    <p></p>
+                    <img></img>
+                </div>
+
+                <div>
+                    <p></p>
+                    <img></img>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
